eb5b29867d043a8ea2efd43abce8b85949c4cd47
Sai Nikhil <tsnlegend@gmail.com>
2021-05-21 13:32:36 -0400

    Advanced Data Science Specialization - IBM specialization stated. Doing course-1, "Fundaments of Scalable Data Science" right now

--- data-structures-algorithms/data-structures/assignments/src/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ data-structures-algorithms/data-structures/assignments/src/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -13,18 +13,19 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append((next, i))
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) == 0:
+                return i + 1
+            left = opening_brackets_stack.pop(-1)
+            if not are_matching(left[0], next):
+                return i + 1
+    return "Success" if len(opening_brackets_stack) == 0 else opening_brackets_stack.pop(-1)[1] + 1
 
 
 def main():
     text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(find_mismatch(text))
 
 
 if __name__ == "__main__":
--- data-structures-algorithms/data-structures/assignments/src/week1_basic_data_structures/2_tree_height/tree_height.py
+++ data-structures-algorithms/data-structures/assignments/src/week1_basic_data_structures/2_tree_height/tree_height.py
@@ -4,8 +4,7 @@ import sys
 import threading
 
 
-def compute_height(n, parents):
-    # Replace this code with a faster implementation
+def compute_height_naive(n, parents):
     max_height = 0
     for vertex in range(n):
         height = 0
@@ -17,15 +16,42 @@ def compute_height(n, parents):
     return max_height
 
 
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, node):
+        self.children.append(node)
+
+
+def compute_height(n, parents):
+    nodes = [None for _ in range(n)]
+
+    for i in range(n):
+        nodes[i] = TreeNode(0)
+
+    for child_index in range(n):
+        parent_index = parents[child_index]
+        if parent_index == -1:
+            root = child_index
+        else:
+            nodes[parent_index].add_child(nodes[child_index])
+
+    level = 0
+
+
+
 def main():
     n = int(input())
     parents = list(map(int, input().split()))
     print(compute_height(n, parents))
 
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+if __name__ == "__main__":
+    # In Python, the default limit on recursion depth is rather low,
+    # so raise it here for this problem. Note that to take advantage
+    # of bigger stack, we have to launch the computation in a new thread.
+    sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+    threading.stack_size(2 ** 27)  # new thread will get stack of such size
+    threading.Thread(target=main).start()
